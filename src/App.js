15c67fa01f40c11ee56b16c05f0c993bd974e207
kshitish <kshitish182@gmail.com>
2020-01-17 20:46:26 +0545

    Setup typescript

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import WatchListHome from "./components/WatchListHome";
+
+function App() {
+  return (
+    <WatchListHome />
+  );
+}
+
+export default App;
---
+++ src/components/WatchListHome.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const WatchListHome = () => (
+  <div>content</div>
+);
+
+export default WatchListHome;
